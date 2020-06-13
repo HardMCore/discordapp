a4615d67d96429c157bd2658a35b49e1c1f44b0f
HardMCore <hardmcore@hotmail.com>
2020-06-13 13:33:20 +0200

    bot playing youtube videos

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+client.login('NzIxMjg0Mjc1MDIyMDY5ODAw.XuSb7w.StFgogj_oLXm5Vm8393t0DMCc08');
+
 client.once('ready', () => {
     console.log('Ready!');
+    client.user.setActivity("your mom gay", {
+        type: "STREAMING",
+        url: "twitch.tv",
+    });
 });
 
-client.login('NzIxMjg0Mjc1MDIyMDY5ODAw.XuSb7w.StFgogj_oLXm5Vm8393t0DMCc08');
-client.on('message', message => {
-    console.log(message.content + Discord.);
+
+
+
+
+client.on('message', async message => {
+    // Voice only works in guilds, if the message does not come from a guild,
+    // we ignore it
+    if (!message.guild) return;
+
+    if (message.content === '/join') {
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const ytdl = require('ytdl-core');
+            connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
 });

b05cbc81e4417f6be9a972cbde31550f6daf57e7
HardMCore <hardmcore@hotmail.com>
2020-06-16 13:16:49 +0200

    removed Fs dependency, added 'invalid-url' error catching

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ const ffmpeg = require("ffmpeg-static");
 const ytdl = require("ytdl-core");
 const opusscript = require("opusscript");
 const { getInfo } = require('ytdl-getinfo')
-const fs = require('fs');
 
 const client = new Discord.Client();
 
-var token = JSON.parse(fs.readFileSync('token.json'));
-var commands = JSON.parse(fs.readFileSync('commands.json'));
+var token = require('./token.json')
+var commands = require('./commands.json')
 
 client.login(token.DiscordToken);
 
@@ -22,27 +21,28 @@ client.on('message', async message => {
     var playcmd = commands.play.command;
     if (message.content.startsWith(playcmd)) {
         const args = message.content.slice(playcmd.length).split(' ');
-        console.log(args[1])
         if (args[1] === undefined) {
-            message.reply(commands.play.command);
+            message.reply(commands.play.howto);
             return;
         }
         if (message.member.voice.channel) {
-            try {
-                const connection = await message.member.voice.channel.join();
-                const url = args[1];
-                const video = ytdl(url, {filter: 'audioonly'});
-                connection.play(video);
-                ytdl(url).on('info', (info) => {
-                    console.log(info.length_seconds);
-                });
-            }
-            catch(err) {
-                /*nothing*/
-            }
-            finally {
+            const url = args[1];
+            var validurl = ytdl.validateURL(url);
+
+            console.log(validurl);
+
+            if(validurl === false) {
                 message.reply(commands.voiceerror);
+                return;
             }
+
+            const video = ytdl(url, {filter: 'audioonly'});
+
+            const connection = await message.member.voice.channel.join();
+            connection.play(video);
+            ytdl(url).on('info', (info) => {
+                console.log(info.length_seconds);
+            });
         } else {
             message.reply(commands.mustbe);
         }
@@ -51,11 +51,11 @@ client.on('message', async message => {
 
 client.on('message', async message => {
     if (!message.guild) return;
-    var searchcmd = "/search"
+    var searchcmd = commands.search.command
     if (message.content.startsWith(searchcmd)) {
         const args = message.content.slice(searchcmd.length);
         if (args === undefined){
-            message.reply('Podaj nazwę piosenki! (/search [nazwa])');
+            message.reply(commands.search.howto);
             return;
         }
         getInfo(args).then(async info => {
@@ -69,8 +69,9 @@ client.on('message', async message => {
                 ytdl(url).on('info', (info) => {
                     console.log(info.length_seconds);
                 });
-            } else {
-                message.reply('Musisz być na kanale!');
+            }
+            else {
+                message.reply(commands.mustbe);
             }
         })
 
@@ -86,3 +87,8 @@ client.on('message', async message => {
         message.member.guild.me.voice.kick();
     }
 });
+
+client.on('message', async message => {
+    if (!message.guild) return;
+    var stopcmd = "/stop"
+    if (message.content.startsWith(stopcmd)) {
